7d183ae8b341c3eb96acb9ec320786a72c84cae9
Sipo Charles <sipocharles@yahoo.co.uk>
2018-09-05 06:34:55 +0100

    Moved JS Code Into It's Own Script

---
+++ assets/js/maps.js
@@ -0,0 +1,27 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 48.135124, lng:  11.581981},
+        { lat: 52.486244, lng: -1.890401 },
+        { lat: 51.507351, lng: -0.127758 },
+        { lat: 55.676098, lng: 12.568337 }
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
